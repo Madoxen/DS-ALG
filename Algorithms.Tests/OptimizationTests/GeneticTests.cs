b7f9f19eb600b2d482f69d879fe7081ed34b692d
Madoxen <pchlad5@gmail.com>
2020-05-05 19:09:10 +0200

    Added genetic algorithm

--- Algorithms.Tests/OptimizationTests/GeneticTests.cs
+++ Algorithms.Tests/OptimizationTests/GeneticTests.cs
@@ -3,20 +3,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Algorithms.Tests
 {
     using Lib.Opti;
-    
     [TestClass]
-    public class DifferentialTests
+    public class GeneticTests
     {
         [TestMethod]
         public void Test()
         {
-            Differential d = new Differential();
-            IOptiTestFunc f = new HyperEllipsoid();
-            double[] res = d.Opti(f.Func);
+            Genetic g = new Genetic();
+            IOptiTestFunc f = new Weierstrass();
+            double[] res = g.Opti(f.Func);
             double val = f.Func(res);
-
         }
 
     }
-
 }
